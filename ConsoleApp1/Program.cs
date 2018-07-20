1b85d3b26c66030cc19d00f40869681fbec85766
Venkata Naresh Mudda <naresh.mudda@gmail.com>
2018-07-19 19:57:14 -0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = Person.parse("john");
+            person.Introduce("Mosh");
+        }
+    }
+
+    public class Person
+    {
+        string Name;
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0} I'm {1}", to, Name);
+        }
+
+        public static Person parse(string str)
+        {
+            var person = new Person();
+            person.Name = str;
+
+            return person;
+        }
+    }
+}
